84f6a97367a02baabb1c152c74b6aadd0c926143
Ayush Rai <raiayush3366@gmail.com>
2024-12-29 21:36:50 +0530

    Day 1 of revising and learning JS

---
+++ 1.js-basics.js
@@ -0,0 +1,57 @@
+alert('hello');
+alert('Good job!');
+
+2 + 2
+10 - 3
+
+document.body.innerHTML = 'hello';
+
+//Number and Math
+
+20.95 + 7.99 * 2
+36.93 * 0.1
+(20.95 + 7.99 * 2) * 0.1
+
+// Inaccuracies with Floats:
+0.1 + 0.2
+20.95 + 7.99
+(2095 + 799) / 100
+
+// How to round numbers:
+Math.round(2.2)
+Math.round(2.8)
+
+((2095 + 799) * 0.1) / 100
+Math.round((2095 + 799) * 0.1) / 100
+
+
+
+//STRINGS
+
+'hello'
+alert('hello');
+
+'some' + 'text'
+'some' + 'more' + 'text'
+
+typeof 2
+typeof 'hello'
+
+'hello' + 3
+'$' + 20.95 + 7.99
+'$' + (20.95 + 7.99)
+'$' + (2095 + 799) / 100
+
+'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
+alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+"hello"
+"I'm learning JavaScript"
+
+'I\'m learning JavaScript'
+alert('some\ntext');
+
+`hello`
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+`some
+text`
